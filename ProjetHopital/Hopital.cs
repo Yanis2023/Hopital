7d805cd202a35417085494fcf28abc979a7e6ee1
Yanis <y.chaigneau.pro@gmail.com>
2024-06-03 15:52:35 +0200

    Ajout Class Hopital + debut afficherliste attente

---
+++ ProjetHopital/Hopital.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetHopital
+{
+    class Hopital
+    {
+
+        private static Hopital instance = null;
+        public static Hopital Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Hopital();
+                return instance;
+            }
+        }
+
+        public Queue<Patient> FileAttente { get; private set; }
+        public DaoPatient DaoPatient { get; private set; }
+        public DaoVisite DaoVisite { get; private set; }
+
+        private Hopital()
+        {
+            FileAttente = new Queue<Patient>();
+        }
+    }
+}
--- ProjetHopital/Program.cs
+++ ProjetHopital/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace ProjetHopital
 {
     class Program
     {
+        private static Hopital hopital = Hopital.Instance;
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenue à l'hopital");
@@ -44,9 +46,33 @@ namespace ProjetHopital
                     "4 - Nouvelle journée\n5 - Afficher les visites d'un patient\n6 - Afficher toutes les visites\n" +
                     "7 - Afficher toutes les visites d'un médecin\n8 - Quitter l'interface secrétaire\nVeuillez entrer votre choix: ");
                 while (!Int32.TryParse(Console.ReadLine(), out choix) && (choix < 1 || choix > 8)) ;
-                if (choix == 1)
-                    AjouterPatient();
-                //do redirections there
+                if (choix == 8)
+                    break;
+
+                switch (choix)
+                {
+                    case 1:
+                        AjouterPatient();
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        AfficherFileAttente();
+                        break;
+                    case 4:
+                        
+                        break;
+                    case 5:
+                        
+                        break;
+                    case 6:
+                        
+                        break;
+                    case 7:
+                        
+                        break;
+                }
             }
             Console.WriteLine("Fermeture interface Secrétaire");
         }
@@ -55,7 +81,7 @@ namespace ProjetHopital
         {
             Console.WriteLine("Veuillez saisir un identifiant:");
             int id;
-            while (!Int32.TryParse(Console.ReadLine(), out id));
+            while (!Int32.TryParse(Console.ReadLine(), out id)) ;
             Patient p = (new DaoPatient()).SelectById(id);
             //TODO check si id existe dans la db
             if (p.Id == id)
@@ -89,7 +115,82 @@ namespace ProjetHopital
                 }
                 (new DaoPatient()).Insert(p);
             }
+            hopital.FileAttente.Enqueue(p);
+
+            string dateHeureArrivee = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+            //using (StreamWriter sw = new StreamWriter("patients.txt", true))
+            //{
+            //    sw.WriteLine($"{p.Id} {dateHeureArrivee}");
+            //}
+
+            //Console.WriteLine("Patient ajouté à la file d'attente avec succès.");
         }
+
+        private static void AfficherFileAttente()
+        {
+            string filePath = "patients.txt";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Le fichier patients.txt n'existe pas.");
+                return;
+            }
+
+            Console.WriteLine("Liste des patients dans la file d'attente :");
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                    string[] parts = line.Split(' ');
+
+                   
+
+                }
+            }
+        }
+
+        private static void AfficherProchainPatient()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        private static void MettreAJourPatient()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        private static void AfficherVisitesPatient()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+
+        private static void AfficherToutesLesVisites()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        private static void AfficherToutesVisitesDUnMedecin()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+
 
         //interface Médecin
 
