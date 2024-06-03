7b09307b5a62f4efed4e3d3b2d471ae9be54b8c3
Camille <turtuless@hotmail.fr>
2024-06-03 11:46:11 +0200

    Add file to handle sql server info for login (will be added to the gitignore once everyone has it)

---
+++ ProjetHopital/InfoSql.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetHopital
+{
+    public class InfoSql
+    {
+        public const string CONNEXION_INFO = @"Data Source=PC-YOANN-23\SQLEXPRESS;Initial Catalog=ajc;Integrated Security=True";
+    }
+}
